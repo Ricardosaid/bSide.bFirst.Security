29f7ab01ea14243b524f7ffc6ada748a4fa5302b
RicardoSMS <rsmartinez@bside.com.mx>
2021-12-13 20:04:58 -0600

    Initialized repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bSide.bFirst.Security
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
