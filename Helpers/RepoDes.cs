d1572bd7e19fe3a7039479b15eee5d03c43d1869
RicardoSMS <rsmartinez@bside.com.mx>
2021-12-17 02:07:34 -0600

    RepoDes created
    Deserealization the JSON

---
+++ Helpers/RepoDes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bSide.bFirst.Security.Helpers
+{
+    /*
+     * Definimos una clase para representar el objeto JSON devuelto desde la API
+     * de query. Usará esta clase para mostrar una lista de las CardsNumber de repositorio. 
+     * Por lo que trabajaremos con un subconjunto de campos de un paquete JSon
+     
+     */
+    public class RepoDes
+    {
+        public int cardNumber { get; set; } //La convención es que sea mayuscula la primera letra, pero en el JSOn la tenemos así
+    }
+}
